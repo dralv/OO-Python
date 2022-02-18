8e358454c683ecf3dca0fe72414391289989c5c6
Álvaro Teixeira de Araújo <dralvphd2@gmail.com>
2022-02-18 13:50:20 -0300

    Métodos privados
    Colocar "__" nos métodos que devem ser acessados apenas dentro da classe

--- OO/conta.py
+++ OO/conta.py
@@ -13,17 +13,26 @@ class Conta:
     def deposita(self,valor):
         self.__saldo += valor
 
+    def __pode_sacar(self,valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca(self,valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self,valor,destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
     @property
